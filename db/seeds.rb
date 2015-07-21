6e61380368ad0f9401bae3c993838b949429aec8
Ryan Smith <ryanc.smith27@gmail.com>
2015-07-21 14:47:20 -0700

    Added new drawings, fixed basic navbar css

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # ruby encoding: utf-8
 
-Drawing.create(name: 'Screw and Nut', description: 'Screw and Nut 12cm tall', res: 'http://www.fabster.com/media/gallery/d/59/normal_IMG_2376.jpg', origin: 'http://www.fabster.com/physible/PHY000262/screw-and-nut')
+Drawing.create(name: 'Chess Set', description: 'Full chess set', res: 'http://thingiverse-production-new.s3.amazonaws.com/renders/85/40/c9/e9/29/acs-topview_preview_featured.jpg', origin: 'https://www.thingiverse.com/thing:923706')
 Drawing.create(name: 'Eiffel Tower Miniature', description: 'Mini Eiffel Tower from Paris, France', res: 'https://static.pinshape.com/s3-proxy/public/uploads/image/file/8396/container_eiffel-tower-miniature-paris-france-3d-printing-8396.jpg', origin: 'https://pinshape.com/items/2077-3d-printed-eiffel-tower-miniature-paris-france')
-Drawing.create(name: 'Happy 2015 Keychain', description: 'Keychain to wish happy 2015.', res: 'http://thingiverse-production.s3.amazonaws.com/renders/bd/9e/20/b5/9c/happy2015key_preview_featured.jpg', origin: 'http://www.thingiverse.com/thing:616960')
+Drawing.create(name: 'Medieval Castle', description: 'Model medieval castle', res: 'http://thingiverse-production-new.s3.amazonaws.com/renders/50/09/ee/11/6d/Castle_002_preview_featured.JPG', origin: 'http://www.thingiverse.com/thing:862724')
