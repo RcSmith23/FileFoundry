cfc7bbe78374db4f99d9862e2288a3f53fdb5a40
Ryan Smith <ryanc.smith27@gmail.com>
2015-07-27 14:35:38 -0700

    Search features

--- app/models/drawing.rb
+++ app/models/drawing.rb
@@ -4,4 +4,11 @@ class Drawing < ActiveRecord::Base
     paginate :per_page => 15, :page => page
   end
 
+  def self.search_async(search)
+    if search
+      where('name LIKE ? ', "%#{search}%")
+    else
+      scoped
+  end
+
 end
