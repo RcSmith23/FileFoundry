56d8e1c0cc7bf63353c3b14733d10ccad996266c
Ryan Smith <ryanc.smith@outlook.com>
2015-08-19 15:43:27 -0700

    Made tredning loop around

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,15 +3,19 @@ class WelcomeController < ApplicationController
     if not params.has_key?(:page)
       @trending = Drawing.all.paginate(page: 1, per_page: 3)
     elsif params[:page].to_i < 1
-      @trending = Drawing.all.paginate(page: 1, per_page: 3)
+      @trending = Drawing.all.page(0).per_page(3)
     else
-      @trending = Drawing.all.paginate(page: params[:page], per_page: 3)
+      @trending = Drawing.all.page(params[:page]).per_page(3)
     end
     @categories = %w(science technology hardware)
     @models = Drawing.take(4)
 
     if params.has_key?(:search)
       redirect_to action: 'search', :search => params[:search]
+    end
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
 
